2a287164d2d44fc3f03f5839f387001ca994c115
Asachi <leonaasachi@gmail.com>
2024-02-25 13:03:54 +0300

    user details

--- user-input.py
+++ user-input.py
@@ -1,4 +1,4 @@
 name = input('What is your name: ')
 age = input('How old are you?: ')
 location = input('What is your location?: ')
-print('Hello '+ name + ', you are '+ age + ' dyears old and your current location is ' + location)
+print('Hello '+ name + ', you are '+ age + ' years old and your current location is ' + location)
